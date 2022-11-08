6a50ee936e351f3df5d24180e3df8b3932ecc736
+ <+>
2022-11-08 13:52:08 -0600

    commit tree c3ab4eef29c03c68a61966113a293dcc288e15c4

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+function writeCards(invites, event) {
+  const newArray = [];
+  for (let i = 0; i < invites.length; i++) {
+    newArray.push(`Thank you, ${invites[i]}, for the wonderful ${event} gift!`);
+  }
+  return newArray;
+}
+writeCards(["Guadalupe", "Ollie", "Aki"], "birthday");
+
+function countDown() {
+  let countDown = 10;
+  while (countDown > -1) {
+    console.log(countDown--);
+  }
+}
+
+countDown();
